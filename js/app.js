387cfb2f52e03ff0b73b62c4c6c32881cd352c9f
venom <hhnvenom@gmail.com>
2026-09-16 09:09:03 +0700

    Đồng bộ code demo với slide (tách biến, ngắt dòng dài cho vừa khung slide)

--- js/app.js
+++ js/app.js
@@ -13,8 +13,11 @@ function openStudentInfo() {
   const left = (screen.width - width) / 2
   const top = (screen.height - height) / 2
 
-  const w = window.open('', 'StudentInfo',
-    `width=${width},height=${height},top=${top},left=${left}`)
+  const opts =
+    `width=${width},height=${height},` +
+    `top=${top},left=${left}`
+
+  const w = window.open('', 'StudentInfo', opts)
 
   w.document.write(`
     <h1>Thông tin sinh viên</h1>
